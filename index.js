0ce9034f05ddfaf94bc3ab449c6feb66e89cbd13
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-06 23:27:39 +0100

    Routing added

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ app.listen(port, () => {
 });
 
 // response is same for every request and on every route
-app.use((req, res) => {
-  res.send("This is a basic response.");
-});
+// app.use((req, res) => {
+//   res.send("This is a basic response.");
+// });
 
 // it will convert object to json representation
 // app.use((req, res) => {
 //   res.send({ name: "apple", color: "red" });
 // });
+
+app.get("/", (req, res) => {
+  res.send("you are on root path.");
+});
+
+app.get("/apple", (req, res) => {
+  res.send("you are on apple path.");
+});
+
+app.get("/orange", (req, res) => {
+  res.send("you are on orange path.");
+});
+app.get("*", (req, res) => {
+  res.send("this path doesnot exist.");
+});
